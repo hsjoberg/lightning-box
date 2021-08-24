35dcf832dc955a64b88920d5236317c8ee8ec714
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2021-08-25 00:13:42 +0200

    Add config_TEMPLATE.ts

---
+++ config/config_TEMPLATE.ts
@@ -0,0 +1,17 @@
+import { Config } from "./interface";
+
+// Please see interface.ts for description about each config
+const config: Config = {
+  env: "prod",
+  serverHost: "0.0.0.0:8080",
+  domain: "192.168.1.1:8080",
+  domainUrl: "http://192.168.1.1:8080",
+  backend: "lnd",
+  backendConfigLnd: {
+    grpcServer: "127.0.0.1:10007",
+    cert: "~/path/to/lnd/tls.cert",
+    adminMacaroon: "~/path/to/lnd/admin.macaroon",
+  },
+};
+
+export default config;
